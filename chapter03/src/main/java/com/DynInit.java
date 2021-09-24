acd1903ec8690995bb5a4eb1bf16ec262db888db
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-24 19:03:24 +0300

    Add files via upload

---
+++ chapter03/src/main/java/com/DynInit.java
@@ -0,0 +1,16 @@
+
+import java.lang.Math;
+
+public class DynInit {
+
+    public static void main(String[] args){
+
+        double a = 3.0, b = 4.0;
+        double c;
+        c= Math.sqrt(a*a+b*b);
+
+        System.out.println("Гипотенуза равна "+c);
+
+
+    }
+}
