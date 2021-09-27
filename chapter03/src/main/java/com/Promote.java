bf511ba267ee778766d3a7fe2431fdbf15ae9773
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-27 13:04:57 +0300

    Add files via upload

---
+++ chapter03/src/main/java/com/Promote.java
@@ -0,0 +1,16 @@
+public class Promote {
+
+    public static void main(String[] agrs){
+
+        byte b=42;
+        char c = 'a';
+        short s =1024;
+        int i=50000;
+        float f = 5.67f;
+        double d = .1234;
+        double result = (f*b)+(i/c)-(d*s);
+        System.out.println((f*b)+" + "+(i/c)+" - "+(d*s));
+        System.out.println("result = "+ result);
+
+    }
+}
