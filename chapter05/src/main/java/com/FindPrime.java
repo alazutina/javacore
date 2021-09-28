b989411f4e969ee6706a532886de0a153e8834cd
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-28 13:09:50 +0300

    Add files via upload

---
+++ chapter05/src/main/java/com/FindPrime.java
@@ -0,0 +1,19 @@
+public class FindPrime {
+    public static void main(String[] args){
+        int num;
+        boolean isPrime;
+
+        num = 11;
+        if(num<2) isPrime = false;
+        else isPrime = true;
+
+        for(int i=2;i<=num/i;i++){
+            if((num%i)==0){
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime)System.out.println("простое число");
+        else System.out.println("Не простое число");
+    }
+}
