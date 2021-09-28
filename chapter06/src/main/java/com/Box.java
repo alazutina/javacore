ed87aee5b6c5165c17a5880dd94419542afc2234
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-28 14:51:06 +0300

    Add files via upload

--- chapter06/src/main/java/com/Box.java
+++ chapter06/src/main/java/com/Box.java
@@ -2,4 +2,10 @@ public class Box {
     double width;
     double height;
     double depth;
+
+    void volume(){
+        System.out.print("Объем равен ");
+        System.out.println(width*height*depth);
+    }
+
 }
