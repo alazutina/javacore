3d7d7f414d3b2689ecdfbe8c293a1df4bed71f48
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-28 14:13:43 +0300

    Add files via upload

---
+++ chapter05/src/main/java/com/BreakLoop4.java
@@ -0,0 +1,17 @@
+public class BreakLoop4 {
+    public static void main(String[] args){
+
+        outer:   for(int i=0;i<3;i++){
+            System.out.print("Проход "+i+": ");
+            for (int j=0;j<100;j++){
+
+                if(j==10)break outer;
+                System.out.print(j+" ");
+            }
+            System.out.println("эта строка выводиться не будет");
+
+        }
+        System.out.println("Циклы завершины.");
+
+    }
+}
