191537b03779ad32030ae9de436ee919a1d8afab
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-28 13:24:45 +0300

    Add files via upload

--- chapter05/src/main/java/com/Comma.java
+++ chapter05/src/main/java/com/Comma.java
@@ -6,7 +6,7 @@ public class Comma {
             for(a=1 , b=4;a<b;a++, b--){
                 System.out.println("a = "+a);
                 System.out.println("b = "+b);
-              
+
             }
 
 
