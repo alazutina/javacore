dfb616f895360ae6abb300beb91d3bf4bc3dd6f9
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-29 13:40:37 +0300

    Add files via upload

---
+++ chapter07/src/main/java/com/Stack.java
@@ -0,0 +1,27 @@
+public class Stack {
+  //  public static void main(String[] args){
+        private int stck[] = new int[10];
+        private int tos;
+
+        Stack(){
+            tos = -1;
+        }
+        void push(int item){
+            if(tos==9)
+                System.out.println("Стек заполнен");
+            else
+                stck[++tos]=item;
+
+        }
+
+        int pop(){
+
+            if(tos<0){
+
+                System.out.println("Стек не загружен");
+                return 0;
+            }
+            else return stck[tos--];
+        }
+   // }
+}
