11354d0bfb1e985a6984a29d4d0339cd68dee096
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-29 17:11:23 +0300

    Add files via upload

---
+++ chapter07/src/main/java/com/VarArgs3.java
@@ -0,0 +1,33 @@
+class VarArgs3 {
+
+
+
+    static void vaTest(int ... v){
+        System.out.print("vaTeset(int...)"+ "Количество аргументов: " +v.length+" Содержимое: ");
+        for (int x:v)
+            System.out.print(x+" ");
+        System.out.println();
+    }
+
+    static void vaTest(boolean ... v){
+        System.out.print("vaTeset(boolean...)"+"Количество аргументов: " +v.length+" Содержимое: ");
+        for (boolean x:v)
+            System.out.print(x+" ");
+        System.out.println();
+    }
+    static void vaTest(String msg, int ... v){
+        System.out.print("vaTeset(String, int...)"+msg +v.length+" Содержимое: ");
+        for (int x:v)
+            System.out.print(x+" ");
+        System.out.println();
+    }
+
+
+    public static void main(String[] args){
+      vaTest(1,2,3);
+      vaTest("Проверка: ", 10,20);
+      vaTest(true,false,false);
+    }
+
+
+}
