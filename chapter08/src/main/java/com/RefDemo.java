62c0eeb29ce7967130fcf7bf93e6ecd9d5b6c628
alazutina <86925328+alazutina@users.noreply.github.com>
2021-09-29 18:01:10 +0300

    Add files via upload

---
+++ chapter08/src/main/java/com/RefDemo.java
@@ -0,0 +1,20 @@
+class RefDemo {
+    public static void main(String [] args){
+        BoxWeight weightbox = new BoxWeight(3,5,7,8.37);
+        Box plainbox = new Box();
+        double vol;
+
+        vol = weightbox.volume();
+        System.out.println("Обьем weightbox равен " + vol);
+        System.out.println("Вес weightbox равен "+weightbox.weight);
+        System.out.println();
+
+        plainbox=weightbox;
+
+        vol=plainbox.volume();
+        System.out.println("Обьем plainbox равен " + vol);
+//        System.out.println("Вес plainbox равен "+plainbox.weight);
+//        System.out.println();
+
+    }
+}
